a403aed40a2d71e00101ba19ee0769a1ba894a9c
mayank gautam <mayankgautam2105@gmail.com>
2021-05-06 22:33:24 +0530

    icons fixed

--- pages/_document.js
+++ pages/_document.js
@@ -16,17 +16,19 @@ class MyDocument extends Document {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
 
-          <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="assets/css/animate.min.css" />
-          <link rel="stylesheet" href="assets/css/meanmenu.css" />
-          <link rel="stylesheet" href="assets/css/boxicons.min.css" />
-          <link rel="stylesheet" href="assets/css/magnific-popup.min.css" />
-          <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
-          <link rel="stylesheet" href="assets/css/flaticon.css" />
-          <link rel="stylesheet" href="assets/css/odometer.min.css" />
-          <link rel="stylesheet" href="assets/css/slick.min.css" />
-          <link rel="stylesheet" href="assets/css/style.css" />
-          <link rel="stylesheet" href="assets/css/responsive.css" />
+          <base href="/" />
+
+          <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+          <link rel="stylesheet" href="/assets/css/animate.min.css" />
+          <link rel="stylesheet" href="/assets/css/meanmenu.css" />
+          <link rel="stylesheet" href="/assets/css/boxicons.min.css" />
+          <link rel="stylesheet" href="/assets/css/magnific-popup.min.css" />
+          <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+          <link rel="stylesheet" href="/assets/css/flaticon.css" />
+          <link rel="stylesheet" href="/assets/css/odometer.min.css" />
+          <link rel="stylesheet" href="/assets/css/slick.min.css" />
+          <link rel="stylesheet" href="/assets/css/style.css" />
+          <link rel="stylesheet" href="/assets/css/responsive.css" />
           <title>Distinct Cloud Labs</title>
           <link rel="icon" type="image/png" href="assets/img/favicon.png" />
         </Head>
@@ -37,6 +39,8 @@ class MyDocument extends Document {
             data-cfasync="false"
             src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
           ></script>
+          {/* <script src="https://unpkg.com/feather-icons"></script> */}
+          {/* <script src="path/to/dist/feather.js"></script> */}
           <script src="assets/js/jquery.min.js"></script>
           <script src="assets/js/popper.min.js"></script>
           <script src="assets/js/bootstrap.min.js"></script>
@@ -49,7 +53,7 @@ class MyDocument extends Document {
           <script src="assets/js/imagesloaded.pkgd.min.js"></script>
           <script src="assets/js/isotope.pkgd.min.js"></script>
           <script src="assets/js/owl.carousel.min.js"></script>
-          <script src="assets/js/feather.min.js"></script>
+          {/* <script src="assets/js/feather.min.js"></script> */}
           <script src="assets/js/form-validator.min.js"></script>
           <script src="assets/js/contact-form-script.js"></script>
           <script src="assets/js/startp-map.js"></script>
